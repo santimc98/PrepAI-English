88d0634358cce80c5fe0746046c8334ab5ca7210
santimc98 <santi_serrallo@hotmail.es>
2025-08-12 12:46:05 +0200

    chore(eslint): add .eslintrc.cjs to repo

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: { es2021: true, node: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
+  settings: { react: { version: 'detect' } },
+  ignorePatterns: [],
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'no-empty': ['error', { allowEmptyCatch: true }],
+  },
+};
